5194e6ee9a6d469ebba41cf27f2fe39628e22df9
Martin Lillieroth <martin.lillieroth@gmail.com>
2025-09-10 16:58:56 +0200

    show which ipa

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 cd -P "$(dirname "$0")" || exit 1
 
+echo "$(ls -d ../archive/* | tail -1)/wip.ipa"
+
 cp "$(ls -d ../archive/* | tail -1)/wip.ipa" poc-app.ipa
 git add -u
 git commit -m "new version"
